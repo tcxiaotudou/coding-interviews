17aab8186161b40469bc28be8ce4bf42a3e175d5
tcxiaotudou <2290262044@qq.com>
2020-11-10 18:02:52 +0800

    JZ49

---
+++ src/nowcoder/coding_interviews/JZ49.java
@@ -0,0 +1,20 @@
+package nowcoder.coding_interviews;
+
+/**
+ 将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0
+ 输入
+ +2147483647
+ 1a33
+ 返回值
+ 2147483647
+ 0
+ */
+public class JZ49 {
+
+    public int StrToInt(String str) {
+        if (str == null || "".equals(str)) {
+            return 0;
+        }
+
+    }
+}
