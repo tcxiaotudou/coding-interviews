350fa649c15abfe5e12f206d6a172df4d5f0c358
tcxiaotudou <2290262044@qq.com>
2020-11-10 15:48:14 +0800

    first commit

---
+++ src/nowcoder/coding_interviews/JZ15.java
@@ -0,0 +1,31 @@
+package nowcoder.coding_interviews;
+
+/**
+ 题目描述
+ 输入一个链表，反转链表后，输出新链表的表头。
+ */
+
+public class JZ15 {
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode ReverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
